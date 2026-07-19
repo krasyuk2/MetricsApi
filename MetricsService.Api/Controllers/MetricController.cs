bb6ace83abcb85c77a677968f64a9da75c481f66
krasyuk2 <krasyuk.maksimka@gmail.com>
2026-07-19 22:08:12 +0700

    Добавлены предпологаемые контроллеры

---
+++ MetricsService.Api/Controllers/MetricController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsApi.Controllers;
+
+/// <summary>
+///     Контроллер для парсинга и получение результатов.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class MetricController : ControllerBase
+{
+    /// <summary>
+    ///     Загрузить файл, провалидировать, распарсить, записать результаты.
+    /// </summary>
+    /// <param name="csvFile"> Файл формата csv. </param>
+    [HttpPost("files")]
+    public async Task<ActionResult> UploadCsvAsync(IFormFile csvFile, CancellationToken ct)
+    {
+        return Ok(); 
+    }
+
+    [HttpGet("results")]
+    public async Task GetResultsAsync(CancellationToken ct)
+    {
+        
+    }
+    
+    [HttpGet("values/{fileName}/latest")]
+    public async Task GetLastTenResultsAsync(string fileName, CancellationToken ct)
+    {
+    }
+}
